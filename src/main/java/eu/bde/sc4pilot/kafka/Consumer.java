495105dee34e3af2d81cb68a4135da69a9446cfe
Luigi Selmi <luigiselmi@gmail.com>
2016-04-14 16:08:50 +0200

    minor changes

--- src/main/java/eu/bde/sc4pilot/kafka/Consumer.java
+++ src/main/java/eu/bde/sc4pilot/kafka/Consumer.java
@@ -10,7 +10,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.io.Resources;
 
