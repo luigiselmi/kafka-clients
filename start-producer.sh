2ad7eb697c7fceb8025ea32860f06b4d2f33c547
Luigi Selmi <luigiselmi@gmail.com>
2021-02-08 14:52:19 +0200

    enabled producer and consumer

--- start-producer.sh
+++ start-producer.sh
@@ -5,5 +5,13 @@ until /kafka/bin/kafka-topics.sh --zookeeper $ZOOKEEPER_SERVERS --list | fgrep -
     sleep 1
 done
 echo XXX $0 OK topic taxi is available
-
-java -jar fcd-producer-1.0.0-jar-with-dependencies.jar producer taxi http://feed.opendata.imet.gr:23577/fcd/gps.json
+echo "KAFKA_CLIENT_TYPE=$KAFKA_CLIENT_TYPE"
+if [[ $KAFKA_CLIENT_TYPE = "producer" ]]
+then 
+  java -jar fcd-producer-1.0.0-jar-with-dependencies.jar producer taxi http://feed.opendata.imet.gr:23577/fcd/gps.json
+elif [[ $KAFKA_CLIENT_TYPE = "consumer" ]]
+then 
+  java -jar fcd-producer-1.0.0-jar-with-dependencies.jar consumer taxi
+else
+  echo "The environment variable KAFKA_CLIENT_TYPE=$KAFKA_CLIENT_TYPE must be producer or consumer"
+fi
